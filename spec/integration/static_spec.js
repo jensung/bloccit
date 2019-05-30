fe3eb7e7a263fa2bd0b190c687c14d6cc7f07708
jensung <jensunngggg@gmail.com>
2019-05-30 11:44:25 -0400

    Assignment 1 Intro to express: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,8 +1,10 @@
+
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
+
   describe("GET /", () => {
     it("should return status code 200", (done) => {
       request.get(base, (err, res, body) => {
@@ -11,4 +13,15 @@ describe("routes : static", () => {
       });
     });
   });
+
+  describe("GET /marco", () =>{
+    it("should return status code 200 and response 'polo'", (done) => {
+      request.get(base + "marco", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toBe("polo")
+        done();
+      });
+    });
+  });
+
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,9 +1,12 @@
-bloccit/src/routes/static.js
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   res.send("Welcome to Bloccit");
+});
+
+router.get("/marco", (req, res, next) => {
+  res.send("polo");
 });
 
 module.exports = router;
