8ff59d72d109aaff811c78db355a8a9d806e9489
jensung <jensunngggg@gmail.com>
2019-05-30 14:28:03 -0400

    Assignment 4 Routing Controllers View: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,12 +1,11 @@
-
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const about = "http://localhost:3000/about";
 
 describe("routes : static", () => {
-
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
@@ -14,15 +13,12 @@ describe("routes : static", () => {
       });
     });
   });
-
-  describe("GET /marco", () =>{
-    it("should return status code 200 and response 'polo'", () => {
-      request.get(base + "marco", (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toBe("polo")
+  describe("GET /about", () => {
+    it("Should have the string 'About Us' in the body", done => {
+      request.get(about, (err, res, body) => {
+        expect(body).toContain("About Us");
         done();
       });
     });
   });
-
 });
