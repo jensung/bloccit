008b7a7330a87111f3f70126b023bf0478d41c6e
jensung <jensunngggg@gmail.com>
2019-05-30 14:10:04 -0400

    Checkpoint 4 Routing Controller View: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,14 +1,28 @@
+
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
+
   describe("GET /", () => {
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
         done();
       });
     });
   });
+
+  describe("GET /marco", () =>{
+    it("should return status code 200 and response 'polo'", () => {
+      request.get(base + "marco", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toBe("polo")
+        done();
+      });
+    });
+  });
+
 });
