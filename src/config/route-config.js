56791a2c559981b08e6302b5a070e4fac01353dd
jensung <jensunngggg@gmail.com>
2019-07-04 04:53:15 -0400

    Checkpoint 11 Authentication: Completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,12 +1,14 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
-    const postRoutes = require("../routes/posts")
+    const postRoutes = require("../routes/posts");
+    const userRoutes = require("../routes/users");
     const topicRoutes = require("../routes/topics");
     const advertisementRoutes = require("../routes/advertisements");
 
     app.use(staticRoutes);
     app.use(postRoutes);
+    app.use(userRoutes);
     app.use(topicRoutes);
     app.use(advertisementRoutes);
   }
---
+++ src/db/migrations/20190605082348-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        //#1 - The validate properly allows for us to pass validators to apply to the data.
+        validate: {
+          isEmail: { msg: "must be a valid email" } // The isEmail validator checks that the value passed to email property is valid and if not surfaces the msg provided.
+        },
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
