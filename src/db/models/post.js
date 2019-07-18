b432a4c65f5667c5abb0997c773a7c2a7c2fe687
jensung <jensunngggg@gmail.com>
2019-07-18 14:10:15 -0400

    Checkpoint 15 Favoriting: Completed

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -36,6 +36,18 @@ module.exports = (sequelize, DataTypes) => {
       as: "comments"
     });
 
+    Post.hasMany(models.Favorite, {
+      foreignKey: "postId",
+      as: "favorites"
+    });
+    
+    Post.afterCreate((post, callback) => {
+      return models.Favorite.create({
+        userId: post.userId,
+        postId: post.id
+      });
+    });
+
     Post.hasMany(models.Vote, {
       foreignKey: "postId",
       as: "votes"
@@ -50,6 +62,10 @@ module.exports = (sequelize, DataTypes) => {
     return this.votes
       .map((v) => { return v.value })
       .reduce((prev, next) => { return prev + next });
+  };
+
+  Post.prototype.getFavoriteFor = function(userId){
+    return this.favorites.find((favorite) => { return favorite.userId == userId });
   };
 
   return Post;
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -29,6 +29,11 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       as: "comments"
     });
+    
+    User.hasMany(models.Favorite, {
+      foreignKey: "userId",
+      as: "favorites"
+    });
 
     User.hasMany(models.Vote, {
       foreignKey: "userId",
